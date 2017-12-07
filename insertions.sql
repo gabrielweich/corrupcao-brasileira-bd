e6420793430f81aa58f6068fe3a3da8986d38e65
Gabriel Weich <gabrielweich@hotmail.com>
2017-12-07 20:35:29 -0200

    Inserções nas tabelas básicas

---
+++ insertions.sql
@@ -0,0 +1,164 @@
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(1, 'Gesiel Oliveira', TO_DATE('1950/05/03 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 88977975042);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(2, 'Alberto Youssef', TO_DATE('1967/10/06 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 99377197635);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(3, 'José Dirceu de Oliveira e Silva', TO_DATE('1946/03/16 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 38262641345);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(4, 'Nestor Cuñat Cerveró', TO_DATE('1951/08/15 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 42004642173);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(5, 'João Vaccari Neto', TO_DATE('1958/10/30 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 84765475875);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(6, 'Paulo Roberto Costa', TO_DATE('1954/04/15 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 25551555542);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(7, 'Marcelo Bahia Odebrecht', TO_DATE('1968/10/18 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 48734775811);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(8, 'Antonio Palocci Filho', TO_DATE('1960/10/04 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 42870136625);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(9, 'Eduardo Cosentino da Cunha', TO_DATE('1958/09/29 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 07124861357);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(10, 'Sérgio Cabral Filho', TO_DATE('1963/01/27 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 81255672404);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(11, 'Aécio Neves da Cunha', TO_DATE('1960/03/10 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 91137367792);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(12, 'Geddel Quadros Vieira Lima', TO_DATE('1959/03/18 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 11446968286);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(13, 'Luiz Inácio Lula da Silva', TO_DATE('1945/10/27 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 46382830470);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(14, 'Renato de Souza Duque', TO_DATE('1955/09/29 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 46764863083);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(15, 'Pedro da Silva Corrêa de Oliveira Andrade Neto', TO_DATE('1948/01/07 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 55970824852);
+
+insert into pessoas(id_pessoa, nome, data_nascimento, cpf) 
+values(16, 'Delcídio do Amaral Gómez', TO_DATE('1948/01/07 00:00:00', 'yyyy/mm/dd hh24:mi:ss'), 64603499671);
+
+insert into cidades(cidade, uf) 
+values('Porto Alegre','RS');
+
+insert into cidades(cidade, uf) 
+values('Londrina','PR');
+
+insert into cidades(cidade, uf) 
+values('Passa Quatro','MG');
+
+insert into cidades(cidade, uf) 
+values('Rio de Janeiro','RJ');
+
+insert into cidades(cidade, uf) 
+values('São Paulo','SP');
+
+insert into cidades(cidade, uf) 
+values('Salvador','BH');
+
+insert into cidades(cidade, uf) 
+values('Ribeirão Preto','SP');
+
+insert into cidades(cidade, uf) 
+values('Cascadura','RJ');
+
+insert into cidades(cidade, uf) 
+values('Belo Horizonte','MG');
+
+insert into cidades(cidade, uf) 
+values('Caetés','PE');
+
+insert into cidades(cidade, uf) 
+values('Cruzeiro','SP');
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(1, 'Odebrecht', 'A Organização Odebrecht é um conglomerado empresarial brasileiro de capital fechado que atua nos setores de Engenharia, Infraestrutura, Indústria, Energia, Transportes e Meio Ambiente.', 15101288000182);
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(2, 'Grupo OAS', 'O Grupo OAS é um conglomerado brasileiro que atua em diversos países do mundo no ramo da engenharia civil.', 143105770004);
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(3, 'Petrobras', 'Petróleo Brasileiro S.A. é uma empresa de capital aberto, opera atualmente em 25 países, no segmento de energia, prioritariamente nas áreas de exploração, produção, refino, comercialização e transporte de petróleo, gás natural e seus derivados.', 33000167000101);
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(4, 'Eletrobras', 'A Eletrobras é uma empresa brasileira que atua como uma holding, dividida em geração, transmissão e distribuição, criada em 1962 para coordenar todas as empresas do setor elétrico.', 42540211100167);
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(5, 'Grupo Camargo Corrêa', 'Grupo Camargo Corrêa é um conglomerado de capital fechado que atua nos setores de engenharia e construção, cimento, concessões de energia e de transporte e mobilidade urbana, indústria naval e offshore e incorporação imobiliária.', 61522512000102);
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(6, 'Grupo Andrade Gutierrez', 'Andrade Gutierrez é uma multinacional brasileira que atua nos setores de engenharia, construção, telecomunicações e saneamento', 17262213000194);
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(7, 'Queiroz Galvão', 'O Grupo Queiroz Galvão S.A. é um conglomerado industrial brasileiro que atua nos ramos de Óleo e Gás, Exploração e Produção, Siderurgia, Cimento, Engenharia Ambiental, Participações e Concessões, Desenvolvimento Imobiliário.', 33412792011103);
+
+insert into empresas(id_empresa, nome, descricao, cnpj) 
+values(8, 'Mendes Júnior', 'Mercado de construção pesada no Brasil e no exterior, desenvolvendo projetos nos segmentos de dutos, edificações, eletromecânica, energia, hidrelétricas, indústrias, infra-estrutura, manutenção industrial, petróleo e gás.', 17162082000173);
+
+insert into partidos(id_partido, nome, sigla, numero_eleitoral) 
+values(1, 'Partido dos Trabalhadores', 'PT', 13);
+
+insert into partidos(id_partido, nome, sigla, numero_eleitoral) 
+values(2, 'Partido do Movimento Democrático Brasileiro', 'PMDB', 15);
+
+insert into partidos(id_partido, nome, sigla, numero_eleitoral) 
+values(3, 'Partido da Social Democracia Brasileira', 'PSDB', 45);
+
+insert into partidos(id_partido, nome, sigla, numero_eleitoral) 
+values(4, 'Partido Progressista', 'PP', 11);
+
+insert into codinomes(id_pessoa, codinome) 
+values(1, 'QueHomem');
+
+insert into codinomes(id_pessoa, codinome) 
+values(2, 'Primo');
+
+insert into codinomes(id_pessoa, codinome) 
+values(3, 'Guerrilheiro');
+
+insert into codinomes(id_pessoa, codinome) 
+values(4, 'Zóio');
+
+insert into codinomes(id_pessoa, codinome) 
+values(5, 'Moch');
+
+insert into codinomes(id_pessoa, codinome) 
+values(6, 'PR');
+
+insert into codinomes(id_pessoa, codinome) 
+values(7, 'Pedreiro');
+
+insert into codinomes(id_pessoa, codinome) 
+values(8, 'Italiano');
+
+insert into codinomes(id_pessoa, codinome) 
+values(9, 'Garanguejo');
+
+insert into codinomes(id_pessoa, codinome) 
+values(10, 'Proximus');
+
+insert into codinomes(id_pessoa, codinome) 
+values(11, 'Mineirinho');
+
+insert into codinomes(id_pessoa, codinome) 
+values(12, 'Babel');
+
+insert into codinomes(id_pessoa, codinome) 
+values(13, 'Amigo');
+
+insert into codinomes(id_pessoa, codinome) 
+values(14, 'My Way');
+
+insert into codinomes(id_pessoa, codinome) 
+values(15, 'Maneta');
+
+insert into codinomes(id_pessoa, codinome) 
+values(16, 'Ferrari');
